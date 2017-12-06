d99578e1e9a7bb96174bebef2a17df4557429806
Patrick <piasecki.patrick.15@gmail.com>
2017-12-06 08:18:09 +0100

    added repositories and data.sql

--- src/main/java/pl/piasecki/repositories/CategoryRepository.java
+++ src/main/java/pl/piasecki/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package pl.piasecki.repositories;
 import org.springframework.data.repository.CrudRepository;
 import pl.piasecki.domain.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
